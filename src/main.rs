9100c3aa7394f08612f5fe8caf69afbe6d83f0ac
Yuya Isaka <zxwg6329@gmail.com>
2023-11-30 00:42:56 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,114 @@
+use std::env::Args;
 use std::process::ExitCode;
 
-fn usage(program: &str) {
-    eprintln!("Usage: ./{program} <command>");
-    eprintln!("Commands:");
-    eprintln!("  hello");
-    eprintln!("  reverse <text>");
+struct Command {
+    name: &'static str,
+    description: &'static str,
+    function: fn(Args) -> ExitCode,
 }
 
-fn main() -> std::process::ExitCode {
-    let mut args = std::env::args();
-    let program: String = args.next().expect("Program name not found");
+const COMMANDS: &[Command] = &[
+    Command {
+        name: "hello",
+        description: "Prints 'Hello, world!'",
+        function: hello_command,
+    },
+    Command {
+        name: "reverse",
+        description: "Reverses the given text",
+        function: reverse_command,
+    },
+    Command {
+        name: "uppercase",
+        description: "Converts the given text to uppercase",
+        function: uppercase_command,
+    },
+    Command {
+        name: "lowercase",
+        description: "Converts the given text to lowercase",
+        function: lowercase_command,
+    },
+    Command {
+        name: "help",
+        description: "Prints this help message",
+        function: help_command,
+    },
+];
 
-    if let Some(command) = args.next() {
-        if command == "hello" {
-            println!("Hello, world!");
-        } else if command == "reverse" {
-            let mut flag: bool = false;
-            for arg in args {
-                flag = true;
-                print!("{} ", arg.chars().rev().collect::<String>());
-            }
-            if flag {
-                println!();
-            } else {
-                eprintln!("Error: no text given");
-                usage(&program);
-                return ExitCode::FAILURE;
-            }
+fn usage() {
+    eprintln!("Usage: cargo run <command>");
+    eprintln!("Commands:");
+    for command in COMMANDS {
+        eprintln!(
+            "  {name:12} {description}",
+            name = command.name,
+            description = command.description
+        );
+    }
+}
+
+fn check_given_text(flag: bool) -> ExitCode {
+    if !flag {
+        eprintln!("\nError: no text given\n");
+        usage();
+        return ExitCode::FAILURE;
+    }
+    ExitCode::SUCCESS
+}
+
+fn hello_command(_args: Args) -> ExitCode {
+    println!("Hello, world!");
+    ExitCode::SUCCESS
+}
+
+fn reverse_command(args: Args) -> ExitCode {
+    let mut flag: bool = false;
+    for arg in args {
+        flag = true;
+        let result: String = arg.chars().rev().collect();
+        println!("{result}");
+    }
+    check_given_text(flag)
+}
+
+fn uppercase_command(args: Args) -> ExitCode {
+    let mut flag: bool = false;
+    for arg in args {
+        flag = true;
+        println!("{} ", arg.to_uppercase());
+    }
+    check_given_text(flag)
+}
+
+fn lowercase_command(args: Args) -> ExitCode {
+    let mut flag: bool = false;
+    for arg in args {
+        flag = true;
+        println!("{} ", arg.to_lowercase());
+    }
+    check_given_text(flag)
+}
+
+fn help_command(_args: Args) -> ExitCode {
+    usage();
+    ExitCode::SUCCESS
+}
+
+fn main() -> ExitCode {
+    let mut args: Args = std::env::args();
+    let _program: String = args.next().expect("Program name not found");
+
+    if let Some(command_name) = args.next() {
+        if let Some(command) = COMMANDS.iter().find(|command| command.name == command_name) {
+            (command.function)(args);
         } else {
-            eprintln!("Error: unknown command '{command}'");
-            usage(&program);
+            eprintln!("\nError: unknown command '{command_name}'\n");
+            usage();
             return ExitCode::FAILURE;
         }
     } else {
-        eprintln!("Error: no command given");
-        usage(&program);
+        eprintln!("\nError: no command given\n");
+        usage();
         return ExitCode::FAILURE;
     }
 
